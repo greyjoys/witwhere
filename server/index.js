11d923c4e8161d9b81f070546522950aa33f9ca6
yudataguy <yudataguy@gmail.com>
2019-03-09 10:33:36 -0800

    created root router

--- server/index.js
+++ server/index.js
@@ -1,2 +1,9 @@
 const express = require('express')
 const app = express()
+
+app.get('/', function(req, res, next){
+  // console.log('get route');
+  res.send('<h1>Welcome to WitWhere</h1>');
+});
+
+app.listen(3000, () => console.log("Listening on port 3k"))
