6471dea92a796419a19a84416cae25c425cc2a09
yudataguy <yudataguy@gmail.com>
2019-03-09 12:27:38 -0800

    websocket message and broadcast is working, package.json has new package - express-ws

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
+const path = require('path')
 
-app.get('/', function(req, res, next){
+// app.use(express.static('../build'))
+
+app.ws('/echo', (ws, req) => {
+  ws.on('message', (msg) => { //msg is the string we input
+    ws.send(msg + " received");
+    let connectedUsers = expressWs.getWss('/echo');
+    // console.log('connectedUsers.clients', connectedUsers.clients)
+    connectedUsers.clients.forEach( client => client.send(msg))
+  })
+})
+
+app.get('/build/bundle.js', (req, res) => {
+  res.sendFile(path.resolve('build', 'bundle.js'))
+})
+
+app.get('/', (req, res, next) => {
   // console.log('get route');
-  res.send('<h1>Welcome to WitWhere</h1>');
+  res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
 app.listen(3000, () => console.log("Listening on port 3k"))
