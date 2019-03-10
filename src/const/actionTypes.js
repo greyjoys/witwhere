a9c23bbaa7e6294230b421a2bfe877d954c9c503
Shane Taylor <shaneallantaylor@gmail.com>
2019-03-09 16:59:18 -0800

    initial state set up and addPlayer action created

--- src/const/actionTypes.js
+++ src/const/actionTypes.js
@@ -1,3 +1,17 @@
 // put yer action types here, sailor
+// ARRGGGGHH!
+
+export const UPDATE_PLAYER_NAME = "UPDATE_PLAYER_NAME";
+export const UPDATE_PLAYER_PASS = "UPDATE_PLAYER_PASS";
+export const ADD_PLAYER = "ADD_PLAYER";
+export const UPDATE_P1_ANSWER = "UPDATE_P1_ANSWER";
+export const UPDATE_P2_ANSWER = "UPDATE_P2_ANSWER";
+export const SUBMIT_ANSWER = "SUBMIT_ANSWER";
+export const SET_GAME_READY = "SET_GAME_READY";
+export const SET_TIMER_EXPIRED = "SET_TIMER_EXPIRED";
+export const UPDATE_GAME_STAGE = "UPDATE_GAME_STAGE";
 
 export const TEST_ACTION = "TEST_ACTION"
+// Stretch Goals:
+// export const UPDATE_P1_VOTE_COUNT = 'UPDATE_P1_VOTE_COUNT';
+// export const UPDATE_P2_VOTE_COUNT = 'UPDATE_P2_VOTE_COUNT';
