0e820abbd9313cda0c69d45f19df0912df547042
Ed <15ryane@gmail.com>
2019-03-11 11:58:58 -0700

    implemented basic testing ui - submit now posts to /api/signin/ with a JSON object {username: username, password:password}

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ class App extends Component {
   render() {
     return(
       <div>
-        <h1>This is the top-level App component.</h1>
         <MainContainer />
       </div>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = props => {
 	return(
 		<div className="footer">
-			<h1>This is the Footer component.</h1>
 		</div>
 )}
 
