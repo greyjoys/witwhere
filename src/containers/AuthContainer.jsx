4ddd6822fb6deedca01cd249d8ac64530762620a
justnoah <noahxman@gmail.com>
2019-03-14 10:46:11 -0700

    Consolidated routes to top-level; App.jsx.

--- src/containers/AuthContainer.jsx
+++ src/containers/AuthContainer.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom
 // import actions from action creators file
 import * as actions from '../actions/actions';
 // import from child components when the time comes...
-import Signup from '../components/Signup.jsx';
-import Login from '../components/Login.jsx';
 
 const mapStateToProps = store => ({
   // playerName: store.auth.playerName,
@@ -42,35 +40,29 @@ class AuthContainer extends Component {
   }
 
   loginClicked() {
-    console.log('hello')
     this.setState({ userClickedButton: 'login' })
   }
   
   signUpClicked() {
-    console.log('world')
     this.setState({ userClickedButton: 'signup' })
   }
 
   render() {
-    // if (this.state.)
+
     if (this.state.userClickedButton === 'login') {
       return <Redirect to='/auth/login' />
     }
     if (this.state.userClickedButton === 'signup') {
       return <Redirect to='/auth/signup' />
     }
+    
     return (
       <Router>
-        <React.Fragment>
-          {/* <div className="auth-container">
-            <h1>Inside auth container</h1>
-            <button onClick={ this.loginClicked }>Login</button>
-            <button onClick={ this.signUpClicked }>Sign Up</button>
-          </div> */}
-          <Link to='/auth/login'>Login</Link>
-          <Route path={'/auth/login'} component={ Login } />
-          <Route path={'/auth/signup'} component={ Signup } />
-        </React.Fragment>
+        <div className="auth-container">
+          <h1>Inside auth container</h1>
+          <button onClick={ this.loginClicked }>Login</button>
+          <button onClick={ this.signUpClicked }>Sign Up</button>
+        </div>
       </Router>
     );
   }
